e9052f071a47b03c6a49347ee5aab1760ff540fd
maggie-j-liu <maggie.j.liu@gmail.com>
2021-08-07 20:38:03 -0700

    favicon

--- components/Heading.jsx
+++ components/Heading.jsx
@@ -1,11 +1,12 @@
-import Head from "next/head";
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Heading() {
   return (
     <div className="mx-auto max-w-prose">
-      <h1 className="text-center text-6xl font-bold bg-gradient-to-tr from-sky-400 via-blue-500 to-purple-400 bg-clip-text text-transparent w-max mx-auto">
-        Reactive.
+      <h1 className="hover:italic text-center text-6xl font-bold bg-gradient-to-tr from-sky-400 via-blue-500 to-purple-400 bg-clip-text text-transparent w-max mx-auto">
+        <Link href="https://github.com/maggie-j-liu/reactive">
+          <a>🔥 Reactive.</a>
+        </Link>
       </h1>
       <h2 className="text-center text-3xl">
         An easy to set up reactions and comments widget using your own Firebase
